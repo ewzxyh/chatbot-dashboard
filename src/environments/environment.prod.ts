d4af9502ca2f5b1c4a48295edef5274bcb35069b
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-01-30 13:35:29 +0100

    added: environments - contacts page + contacts service

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: '[REDACTED_GOOGLE_KEY]',
+        authDomain: 'chat-f21.firebaseapp.com',
+        databaseURL: 'https://chat-f21.firebaseio.com',
+        projectId: 'chat-f21',
+        storageBucket: 'chat-f21.appspot.com',
+        messagingSenderId: '573386371492',
+    },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: '[REDACTED_GOOGLE_KEY]',
+        authDomain: 'chat-f21.firebaseapp.com',
+        databaseURL: 'https://chat-f21.firebaseio.com',
+        projectId: 'chat-f21',
+        storageBucket: 'chat-f21.appspot.com',
+        messagingSenderId: '573386371492',
+
+    },
+};
