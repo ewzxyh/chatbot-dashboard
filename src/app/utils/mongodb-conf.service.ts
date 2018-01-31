595dad6858026251982b6f84012f4852b0914af6
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-01-31 19:48:32 +0100

    fix iframe dimensions and chat page

---
+++ src/app/utils/mongodb-conf.service.ts
@@ -0,0 +1,28 @@
+// tslint:disable:max-line-length
+import { Injectable } from '@angular/core';
+
+const MONGODB_CONTACTS_BASE_URL = 'http://localhost:3000/app1/contacts/';
+const TOKEN = 'JWT [REDACTED_JWT]';
+
+
+@Injectable()
+export class MongodbConfService {
+
+  LOCALHOST: true;
+
+  
+
+
+  constructor() {
+
+    if (this.LOCALHOST) {
+
+
+    }
+
+
+  }
+
+
+
+}
