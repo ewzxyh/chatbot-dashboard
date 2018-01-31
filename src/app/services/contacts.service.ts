299901169a6df6e327f6e87cccbf477751ddef7a
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-01-31 16:49:17 +0100

    Completed Mongo DB CRUD

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -15,6 +15,9 @@ export class ContactsService {
     this.http = http;
   }
 
+  /**
+   * READ (GET)
+   */
   public getMongDbContacts(): Observable<Contact[]> {
     const url = `http://localhost:3000/app1/contacts`;
     // const url = `http://api.chat21.org/app1/contacts`;
@@ -29,7 +32,11 @@ export class ContactsService {
       .map((response) => response.json());
   }
 
-  public postMongoDbContacts(fullName: string) {
+  /**
+   * CREATE (POST)
+   * @param fullName
+   */
+  public addMongoDbContacts(fullName: string) {
     const headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-type', 'application/json');
@@ -57,10 +64,16 @@ export class ContactsService {
       });
   }
 
-  public deleteContact(id: string) {
+  /**
+   * DELETE (DELETE)
+   * @param id
+   */
+  public deleteMongoDbContact(id: string) {
 
-    const url = `http://localhost:3000/app1/contacts/{id}# chat21-api-nodejs`;
+    let url = `http://localhost:3000/app1/contacts/`;
+    url += `${id}# chat21-api-nodejs`;
     console.log('DELETE URL ', url);
+
     const headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-type', 'application/json');
@@ -80,6 +93,43 @@ export class ContactsService {
       () => {
         console.log('DELETE REQUEST * COMPLETE *');
       });
+  }
+
+  /**
+   * UPDATE (PUT)
+   * @param id
+   * @param fullName
+   */
+  public updateMongoDbContact(id: string, fullName: string) {
+
+    let url = `http://localhost:3000/app1/contacts/`;
+    url = url += `${id}`;
+    console.log('PUT URL ', url);
+
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-type', 'application/json');
+    headers.append('Authorization', 'JWT [REDACTED_JWT]');
+    const options = new RequestOptions({ headers });
+
+    const body = { 'fullname': `${fullName}` };
+
+    console.log('PUT REQUEST BODY ', body);
+
+    this.http.put(url, JSON.stringify(body), options)
+    .map((res) => res.json())
+    .subscribe((data) => {
+      console.log('PUT DATA ', data);
+    },
+    (error) => {
+
+      console.log('PUT REQUEST ERROR ', error);
+
+    },
+    () => {
+      console.log('PUT REQUEST * COMPLETE *');
+    });
+
   }
 
 }
