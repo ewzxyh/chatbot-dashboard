81a63e188b21d0aa4aae29a85ef719847fa0ca3d
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-03-21 19:49:43 +0100

    added project id in all the url of the app

--- src/app/services/mongodb-department.service.ts
+++ src/app/services/mongodb-department.service.ts
@@ -6,20 +6,22 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 // import { MongodbConfService } from '../utils/mongodb-conf.service';
 import { environment } from '../../environments/environment';
+import { AuthService } from '../core/auth.service';
 
 @Injectable()
 export class MongodbDepartmentService {
 
   http: Http;
 
-  // MONGODB_BASE_URL: any;
-  // TOKEN: any;
   MONGODB_BASE_URL = environment.mongoDbConfig.DEPARTMENTS_BASE_URL;
-  TOKEN =  environment.mongoDbConfig.TOKEN;
+  // TOKEN =  environment.mongoDbConfig.TOKEN;
+  TOKEN: string
+  user: any;
 
   constructor(
     http: Http,
     // private mongodbConfService: MongodbConfService,
+    private auth: AuthService
   ) {
 
     this.http = http;
@@ -27,13 +29,31 @@ export class MongodbDepartmentService {
     // this.MONGODB_BASE_URL = mongodbConfService.MONGODB_DEPARTMENTS_BASE_URL;
     // console.log('MONGODB_DEPARTMENTS_BASE_URL ! ', this.MONGODB_BASE_URL);
     // this.TOKEN = mongodbConfService.TOKEN;
+
+    // SUBSCRIBE TO USER BS
+    this.user = auth.user_bs.value
+    this.checkUser()
+
+    this.auth.user_bs.subscribe((user) => {
+      this.user = user;
+      this.checkUser()
+    });
+  }
+
+  checkUser() {
+    if (this.user) {
+      this.TOKEN = this.user.token
+      // this.getToken();
+    } else {
+      console.log('No user is signed in');
+    }
   }
 
   /**
    * READ (GET)
    */
   // headers.append('Authorization', 'JWT [REDACTED_JWT]');
-    // headers.append('Authorization', 'JWT [REDACTED_JWT]');
+  // headers.append('Authorization', 'JWT [REDACTED_JWT]');
   public getMongDbDepartments(): Observable<Department[]> {
     const url = this.MONGODB_BASE_URL;
     // const url = `http://localhost:3000/app1/departments/`;
@@ -51,18 +71,18 @@ export class MongodbDepartmentService {
  * READ DETAIL (GET BOT BY BOT ID)
  * @param id
  */
-public getMongDbDeptById(id: string): Observable<Department[]> {
-  let url = this.MONGODB_BASE_URL;
-  url += `${id}`;
-  console.log('MONGO DB GET DEPT BY DEPT ID URL', url);
+  public getMongDbDeptById(id: string): Observable<Department[]> {
+    let url = this.MONGODB_BASE_URL;
+    url += `${id}`;
+    console.log('MONGO DB GET DEPT BY DEPT ID URL', url);
 
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Authorization', this.TOKEN);
-  return this.http
-    .get(url, { headers })
-    .map((response) => response.json());
-}
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', this.TOKEN);
+    return this.http
+      .get(url, { headers })
+      .map((response) => response.json());
+  }
 
   /**
    * CREATE (POST)
@@ -75,7 +95,7 @@ public getMongDbDeptById(id: string): Observable<Department[]> {
     headers.append('Authorization', this.TOKEN);
     const options = new RequestOptions({ headers });
 
-    const body = { 'name': `${deptName}`, 'id_bot': `${id_bot}`, 'routing': `${routing}`  };
+    const body = { 'name': `${deptName}`, 'id_bot': `${id_bot}`, 'routing': `${routing}` };
 
     console.log('POST REQUEST BODY ', body);
 
@@ -153,17 +173,17 @@ public getMongDbDeptById(id: string): Observable<Department[]> {
     return this.http
       .put(url, JSON.stringify(body), options)
       .map((res) => res.json());
-      // .subscribe((data) => {
-      //   console.log('PUT DATA ', data);
-      // },
-      // (error) => {
+    // .subscribe((data) => {
+    //   console.log('PUT DATA ', data);
+    // },
+    // (error) => {
 
-      //   console.log('PUT REQUEST ERROR ', error);
+    //   console.log('PUT REQUEST ERROR ', error);
 
-      // },
-      // () => {
-      //   console.log('PUT REQUEST * COMPLETE *');
-      // });
+    // },
+    // () => {
+    //   console.log('PUT REQUEST * COMPLETE *');
+    // });
 
   }
 
