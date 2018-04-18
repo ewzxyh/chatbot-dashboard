d04d31f95dfbe7545522008107fe071d43a78b20
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-04-18 19:36:45 +0200

    fixed update firebase email and password

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,18 +10,27 @@ export const environment = {
         messagingSenderId: '77360455507',
     },
     mongoDbConfig: {
-        BASE_URL: 'http://api.chat21.org/',
+        // BASE_URL: 'http://api.chat21.org/',
+        // PROJECTS_BASE_URL: 'http://api.chat21.org/projects/',
+        // SIGNUP_BASE_URL: 'http://api.chat21.org/auth/signup',
+        // SIGNIN_BASE_URL: 'http://api.chat21.org/auth/signin',
+        // FIREBASE_SIGNIN_BASE_URL: 'http://api.chat21.org/firebase/auth/signin',
+
+        // NEW IS HTTPS
+        BASE_URL: 'https://chat21-api-nodejs.herokuapp.com/',
+        PROJECTS_BASE_URL: 'https://chat21-api-nodejs.herokuapp.com/projects/',
+        SIGNUP_BASE_URL: 'https://chat21-api-nodejs.herokuapp.com/auth/signup',
+        SIGNIN_BASE_URL: 'https://chat21-api-nodejs.herokuapp.com/auth/signin',
+        FIREBASE_SIGNIN_BASE_URL: 'https://chat21-api-nodejs.herokuapp.com/firebase/auth/signin',
+
+
         // DEPARTMENTS_BASE_URL: 'http://api.chat21.org/app1/departments/', // URL BUILT directly IN DEPARTMENTS SERVICE
         // FAQKB_BASE_URL: 'http://api.chat21.org/app1/faq_kb/', // URL BUILT directly IN FAQ-KB SERVICE
         // FAQ_BASE_URL: 'http://api.chat21.org/app1/faq/', // URL BUILT directly IN FAQ SERVICE
         CONTACTS_BASE_URL: 'http://api.chat21.org/app1/contacts/',
         BOTS_BASE_URL: 'http://api.chat21.org/app1/bots/',
         // PROJECT_USER_BASE_URL: 'http://api.chat21.org/app1/project_users/', // NO MORE USED - THE RELATION PROJECT -> PROJECT USER IT'S DONE chat21-api-node.js
-        PROJECTS_BASE_URL: 'http://api.chat21.org/projects/',
-        SIGNUP_BASE_URL: 'http://api.chat21.org/auth/signup',
-        SIGNIN_BASE_URL: 'http://api.chat21.org/auth/signin',
         MONGODB_PEOPLE_BASE_URL: 'http://api.chat21.org/app1/people/',
-        FIREBASE_SIGNIN_BASE_URL: 'http://api.chat21.org/firebase/auth/signin',
         TOKEN: 'JWT [REDACTED_JWT]',
     },
     cloudFunctions: {
