a83839143548e6fe5db98e45ba48d91a0063149f
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-09-19 18:20:22 +0200

    ANALYTICS: added the card 'last month requests count'

--- src/app/services/requests.service.ts
+++ src/app/services/requests.service.ts
@@ -657,18 +657,36 @@ export class RequestsService {
 
   public requestsByDay() {
     // USED TO TEST (note: this service doen't work in localhost)
-    // const url = 'https://api.tiledesk.com/v1/' + '5b44c82def5dca0014d777ac' + '/analytics/requests/aggregate/day';
-    const url = this.BASE_URL + this.project._id + '/analytics/requests/aggregate/day';
+    const url = 'https://api.tiledesk.com/v1/' + '5b44c82def5dca0014d777ac' + '/analytics/requests/aggregate/day';
+    // const url = this.BASE_URL + this.project._id + '/analytics/requests/aggregate/day';
     console.log('!!! ANALYTICS - REQUESTS BY DAY - URL ', url);
 
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     // USED TO TEST (note: this service doen't work in localhost)
-    // headers.append('Authorization', 'JWT [REDACTED_JWT]');
-    headers.append('Authorization', this.TOKEN);
+    headers.append('Authorization', 'JWT [REDACTED_JWT]');
+    // headers.append('Authorization', this.TOKEN);
     return this.http
       .get(url, { headers })
       .map((response) => response.json());
   }
+
+  // https://api.tiledesk.com/v1/5ab0f32757066e0014bfd718/analytics/requests/count
+  public lastMonthRequetsCount() {
+   // USED TO TEST (note: this service doen't work in localhost)
+   const url = 'https://api.tiledesk.com/v1/' + '5b44c82def5dca0014d777ac' + '/analytics/requests/count';
+   // const url = this.BASE_URL + this.project._id + '/analytics/requests/count';
+   console.log('!!! ANALYTICS - REQUESTS BY DAY - URL ', url);
+
+   const headers = new Headers();
+   headers.append('Content-Type', 'application/json');
+   // USED TO TEST (note: this service doen't work in localhost)
+   headers.append('Authorization', 'JWT [REDACTED_JWT]');
+   // headers.append('Authorization', this.TOKEN);
+   return this.http
+     .get(url, { headers })
+     .map((response) => response.json());
+  }
+
 
 }
