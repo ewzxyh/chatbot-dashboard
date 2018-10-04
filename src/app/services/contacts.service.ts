e3f98a5bc23501537b2754d7b20e8b477f1cf654
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-10-04 12:11:03 +0200

    CONTACTS: in progress

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -70,17 +70,22 @@ export class ContactsService {
 
 
   // GET LEADS
-  public getLeads(): Observable<Contact[]> {
-    // const url = this.BASE_URL + this.projectId + '/leads';
+  public getLeads(querystring, pagenumber): Observable<Contact[]> {
+
+    let _querystring = '&' + querystring
+    if (querystring === undefined || !querystring) {
+      _querystring = ''
+    }
+    const url = this.BASE_URL + this.projectId + '/leads?page=' + pagenumber + _querystring;
     // use this to test
-    const url = 'https://api.tiledesk.com/v1/5ba35f0b9acdd40015d350b6/leads'
+    // const url = 'https://api.tiledesk.com/v1/5ba35f0b9acdd40015d350b6/leads?page=' + pagenumber + _querystring;
     console.log('!!!! CONTACTS SERVICE - GET CONTACTS URL', url);
 
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // headers.append('Authorization', this.TOKEN);
+    headers.append('Authorization', this.TOKEN);
     // use this to test
-    headers.append('Authorization', 'JWT [REDACTED_JWT]');
+    // headers.append('Authorization', 'JWT [REDACTED_JWT]');
 
     return this.http
       .get(url, { headers })
