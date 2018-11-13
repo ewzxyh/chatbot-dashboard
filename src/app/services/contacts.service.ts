14293a32f0446c1e6fdd5876e0058385d0f8563b
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-11-13 19:45:36 +0100

    Added delete and update contact

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -55,7 +55,6 @@ export class ContactsService {
     })
   }
 
-
   checkUser() {
     if (this.user) {
       this.TOKEN = this.user.token
@@ -68,7 +67,6 @@ export class ContactsService {
     }
   }
 
-
   // GET LEADS
   public getLeads(querystring, pagenumber): Observable<Contact[]> {
     let _querystring = '&' + querystring
@@ -77,13 +75,15 @@ export class ContactsService {
     }
     const url = this.BASE_URL + this.projectId + '/leads?page=' + pagenumber + _querystring;
     // use this to test
-    // const url = 'https://api.tiledesk.com/v1/5ba35f0b9acdd40015d350b6/leads?page=' + pagenumber + _querystring;
+    // 5bcf51dbc375420015542b5f is the id og the project (in production ) progetto test 23 ott of the user redacted@example.invalid
+    // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads?page=' + pagenumber + _querystring;
     console.log('!!!! CONTACTS SERVICE - GET CONTACTS URL', url);
 
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', this.TOKEN);
-    // use this to test
+
+    /****** use this to test *******/
     // headers.append('Authorization', 'JWT [REDACTED_JWT]');
 
     return this.http
@@ -95,14 +95,15 @@ export class ContactsService {
   public getLeadById(id: string): Observable<Contact[]> {
     const url = this.BASE_URL + this.projectId + '/leads/' + id;
 
-    // use this to test
-    // const url = 'https://api.tiledesk.com/v1/5ba35f0b9acdd40015d350b6/leads/' + id;
+    /****** use this to test *******/
+    // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
     console.log('!!!! CONTACTS SERVICE - GET CONTACT BY ID URL', url);
 
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('Authorization', this.TOKEN);
-    // use this to test
+    // headers.append('Authorization', this.TOKEN);
+
+    /****** use this to test *******/
     // headers.append('Authorization', 'JWT [REDACTED_JWT]');
 
     return this.http
@@ -111,105 +112,95 @@ export class ContactsService {
   }
 
   /**
-   * CREATE (POST)
+   * UPDATE (PUT)
+   * @param id
    * @param fullName
    */
-  public addMongoDbContacts(fullName: string) {
+  public updateLead(id: string, fullName: string, _email: string) {
+
+    const url = this.BASE_URL + this.projectId + '/leads/' + id;
+
+    /****** use this to test *******/
+    // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
+    console.log('UPDATE CONTACT URL ', url);
+
     const headers = new Headers();
+
     headers.append('Accept', 'application/json');
     headers.append('Content-type', 'application/json');
     headers.append('Authorization', this.TOKEN);
+
+    /****** use this to test *******/
+    // headers.append('Authorization', 'JWT [REDACTED_JWT]');
+
     const options = new RequestOptions({ headers });
 
-    const body = { 'fullname': `${fullName}` };
+    const body = { 'fullname': fullName, 'email': _email };
+    // const body = {};
+    // if (fullName) {
+    //   body['fullname'] = fullName
+    // }
+    // if (email) {
+    //   body['email'] = email
+    // }
 
-    console.log('POST REQUEST BODY ', body);
-
-    const url = this.MONGODB_BASE_URL;
-
+    console.log('UPDATE CONTACT REQUEST BODY ', body);
     return this.http
-      .post(url, JSON.stringify(body), options)
+      .put(url, JSON.stringify(body), options)
       .map((res) => res.json());
-    // .subscribe((data) => {
-    //   console.log('POST DATA ', data);
-    // },
-    // (error) => {
-
-    //   console.log('POST REQUEST ERROR ', error);
-
-    // },
-    // () => {
-    //   console.log('POST REQUEST * COMPLETE *');
-    // });
   }
 
-  /**
-   * DELETE (DELETE)
-   * @param id
-   */
-  public deleteMongoDbContact(id: string) {
+ /**
+  * DELETE (DELETE)
+  * @param id
+  */
+  public deleteLead(id: string) {
 
-    let url = this.MONGODB_BASE_URL;
-    url += `${id}# chat21-api-nodejs`;
+    const url = this.BASE_URL + this.projectId + '/leads/' + id;
+
+    /****** use this to test *******/
+    // const url = 'https://api.tiledesk.com/v1/5bcf51dbc375420015542b5f/leads/' + id;
+
     console.log('DELETE URL ', url);
 
     const headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-type', 'application/json');
     headers.append('Authorization', this.TOKEN);
+
+    /****** use this to test *******/
+    // headers.append('Authorization', 'JWT [REDACTED_JWT]');
     const options = new RequestOptions({ headers });
     return this.http
       .delete(url, options)
       .map((res) => res.json());
-    // .subscribe((data) => {
-    //   console.log('DELETE DATA ', data);
-    // },
-    // (error) => {
 
-    //   console.log('DELETE REQUEST ERROR ', error);
-
-    // },
-    // () => {
-    //   console.log('DELETE REQUEST * COMPLETE *');
-    // });
   }
 
   /**
-   * UPDATE (PUT)
-   * @param id
+   * CREATE (POST)
    * @param fullName
    */
-  public updateMongoDbContact(id: string, fullName: string) {
+  // public addMongoDbContacts(fullName: string) {
+  //   const headers = new Headers();
+  //   headers.append('Accept', 'application/json');
+  //   headers.append('Content-type', 'application/json');
+  //   headers.append('Authorization', this.TOKEN);
+  //   const options = new RequestOptions({ headers });
 
-    let url = this.MONGODB_BASE_URL;
-    url = url += `${id}`;
-    console.log('PUT URL ', url);
+  //   const body = { 'fullname': `${fullName}` };
 
-    const headers = new Headers();
-    headers.append('Accept', 'application/json');
-    headers.append('Content-type', 'application/json');
-    headers.append('Authorization', this.TOKEN);
-    const options = new RequestOptions({ headers });
+  //   console.log('POST REQUEST BODY ', body);
 
-    const body = { 'fullname': `${fullName}` };
+  //   const url = this.MONGODB_BASE_URL;
 
-    console.log('PUT REQUEST BODY ', body);
+  //   return this.http
+  //     .post(url, JSON.stringify(body), options)
+  //     .map((res) => res.json());
+  // }
 
-    return this.http
-      .put(url, JSON.stringify(body), options)
-      .map((res) => res.json());
-    // .subscribe((data) => {
-    //   console.log('PUT DATA ', data);
-    // },
-    // (error) => {
 
-    //   console.log('PUT REQUEST ERROR ', error);
 
-    // },
-    // () => {
-    //   console.log('PUT REQUEST * COMPLETE *');
-    // });
 
-  }
 
 }
