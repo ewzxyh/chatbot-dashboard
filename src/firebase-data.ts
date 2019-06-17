eb1a844bac187ac3c8cd28be32558bc8a9183ce1
nicolan74 <nicola.lanzilotto@frontiere21.it>
2019-06-17 11:01:42 +0200

    outsourced firebase auth data

---
+++ src/firebase-data.ts
@@ -0,0 +1,18 @@
+export const firebase = {
+  apiKey: '[REDACTED_GOOGLE_KEY]',
+  authDomain: 'chat-v2-dev.firebaseapp.com',
+  databaseURL: 'https://chat-v2-dev.firebaseio.com',
+  projectId: 'chat-v2-dev',
+  storageBucket: 'chat-v2-dev.appspot.com',
+  messagingSenderId: '77360455507',
+};
+
+
+export const firebase_pre = {
+  apiKey: '[REDACTED_GOOGLE_KEY]',
+  authDomain: 'chat21-pre-01.firebaseapp.com',
+  databaseURL: 'https://chat21-pre-01.firebaseio.com',
+  projectId: 'chat21-pre-01',
+  storageBucket: 'chat21-pre-01.appspot.com',
+  messagingSenderId: '269505353043'
+};
