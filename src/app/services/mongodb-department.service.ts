6ba12ee81ee55a56c51a697ca1506701e5fefb91
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-05-18 18:50:33 +0200

    lazy loading departments service

--- src/app/services/mongodb-department.service.ts
+++ src/app/services/mongodb-department.service.ts
@@ -152,8 +152,8 @@ export class DepartmentService {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', this.TOKEN);
-    // tslint:disable-next-line:quotemark
-    // headers.append('Authorization', "JWT [REDACTED_JWT]");
+   
+    
     return this.http
       .get(url, { headers })
       .map((response) => response.json());
@@ -161,18 +161,14 @@ export class DepartmentService {
 
   public getDeptsByProjectIdToPromise():any {
     const url = this.DEPTS_URL + 'allstatus';
-
-    // const url = 'https://api.tiledesk.com/v1/5c28b587348b680015feecca/departments/'+'allstatus'
     console.log('DEPARTMENTS URL', url);
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', this.TOKEN);
-    // tslint:disable-next-line:quotemark
-    // headers.append('Authorization', "JWT [REDACTED_JWT]");
     return this.http
       .get(url, { headers })
       .map((response) => { 
-        // some response manipulation
+      
         return response.json()
       })
       .toPromise();
