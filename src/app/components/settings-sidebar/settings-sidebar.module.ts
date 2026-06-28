72076e4b77a99c6484dc9f538e47bfa59eb644b2
ewzxyh <yoshidaenzo@hotmail.com>
2026-06-28 18:21:58 -0300

    Standardize dashboard page sidebar icons

--- src/app/components/settings-sidebar/settings-sidebar.module.ts
+++ src/app/components/settings-sidebar/settings-sidebar.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SettingsSidebarComponent } from './settings-sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 
@@ -15,6 +16,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     CommonModule,
     FormsModule,
     TranslateModule,
+    MatIconModule,
     MatTooltipModule,
   ],
   exports: [
--- src/app/services/dashboard-icon.service.ts
+++ src/app/services/dashboard-icon.service.ts
@@ -15,25 +15,36 @@ export class DashboardIconService {
   registerIcons(): void {
     this.addTablerIcon('td-activity', 'activity');
     this.addTablerIcon('td-address-book', 'address-book');
+    this.addTablerIcon('td-apps', 'apps');
     this.addTablerIcon('td-bolt', 'bolt');
     this.addTablerIcon('td-brain', 'brain');
     this.addTablerIcon('td-brand-whatsapp', 'brand-whatsapp');
+    this.addTablerIcon('td-building-bank', 'building-bank');
     this.addTablerIcon('td-chart-bar', 'chart-bar');
     this.addTablerIcon('td-chevron-left', 'chevron-left');
     this.addTablerIcon('td-chevron-right', 'chevron-right');
+    this.addTablerIcon('td-clock', 'clock');
     this.addTablerIcon('td-clipboard-list', 'clipboard-list');
+    this.addTablerIcon('td-code', 'code');
     this.addTablerIcon('td-git-branch', 'git-branch');
     this.addTablerIcon('td-history', 'history');
     this.addTablerIcon('td-home', 'home');
+    this.addTablerIcon('td-language', 'language');
     this.addTablerIcon('td-layout-grid', 'layout-grid');
     this.addTablerIcon('td-lifebuoy', 'lifebuoy');
+    this.addTablerIcon('td-mail', 'mail');
     this.addTablerIcon('td-message', 'message');
+    this.addTablerIcon('td-message-bolt', 'message-bolt');
+    this.addTablerIcon('td-plug-connected', 'plug-connected');
     this.addTablerIcon('td-robot', 'robot');
     this.addTablerIcon('td-school', 'school');
     this.addTablerIcon('td-settings', 'settings');
     this.addTablerIcon('td-settings-automation', 'settings-automation');
     this.addTablerIcon('td-shield-cog', 'shield-cog');
+    this.addTablerIcon('td-tag', 'tag');
     this.addTablerIcon('td-template', 'template');
+    this.addTablerIcon('td-tools', 'tools');
+    this.addTablerIcon('td-users', 'users');
     this.addTablerIcon('td-webhook', 'webhook');
   }
 
