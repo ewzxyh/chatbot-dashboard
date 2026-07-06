4428c21247ab512e48427cbfd67fb52f77afe0d1
ewzxyh <yoshidaenzo@hotmail.com>
2026-07-06 18:18:55 -0300

    fix: rename ChatCase dashboard assets

--- src/app/services/chat21-core/utils/constants.ts
+++ src/app/services/chat21-core/utils/constants.ts
@@ -101,8 +101,8 @@ export const STORAGE_PREFIX = 'tiledesk_widget_';
 
 // links
 export const FIREBASESTORAGE_BASE_URL_IMAGE = 'https://firebasestorage.googleapis.com/v0/b/' //+ 'chat-v2-dev.appspot.com/o/';
-export const IMG_PROFILE_BOT = '/assets/images/avatar_bot_tiledesk.svg';
-export const IMG_PROFILE_DEFAULT = '/assets/images/avatar_male_tiledesk.svg';
+export const IMG_PROFILE_BOT = '/assets/img/avatar_bot_chatcase.svg';
+export const IMG_PROFILE_DEFAULT = '/assets/img/no_image_user.png';
 
 //LOG LEVEL
 // export enum LogLevel {
